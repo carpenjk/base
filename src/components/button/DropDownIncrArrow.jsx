7ea50bb785924693015ac0af883afda684613501
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-08-16 11:25:26 -0400

    fix(dropdownincrarrow.jsx): hardcoded css variables and changed next js styles to styled components

--- src/components/button/DropDownIncrArrow.jsx
+++ src/components/button/DropDownIncrArrow.jsx
@@ -1,38 +1,36 @@
 import React from 'react'
-const DropDownIncrArrow = (props) => {
+import styled from 'styled-components'
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+  height: 3rem;
+  width: 100%;
+  background: rgba(255, 255, 255, 0.85);
+  border-radius: 0 0 3px 3px;
+  outline: none;
+  outline-color: transparent;
+  
+  .downArrow {
+    height: 1px;
+    width: 1px;
+    margin-top: 0.9rem;
+    overflow: hidden;
+    border: 0.9rem solid transparent;
+    border-top-color: #767676;
+    border-radius: 5px;
+  }
+  .activeColor {
+    background: #ffffff;
+  }
+`
+const DropDownIncrArrow = () => {
   return (
-    <div className={'arrowContainer '}>
-      <div className="downArrow">Increment Guests</div>
-      <style jsx>
-        {`
-          .arrowContainer {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            align-content: center;
-            height: 3rem;
-            width: 100%;
-            background: rgba(255, 255, 255, 0.85);
-            border-radius: 0 0 3px 3px;
-            outline: none;
-            outline-color: transparent;
-          }
-          .downArrow {
-            height: 1px;
-            width: 1px;
-            margin-top: 0.9rem;
-            overflow: hidden;
-            border: 0.9rem solid transparent;
-            border-top-color: var(--lightText);
-            border-radius: 5px;
-          }
-          .activeColor {
-            background: var(--globalWhite);
-          }
-        `}
-      </style>
-    </div>
+    <StyledContainer className={'arrowContainer '}>
+      <div className="downArrow"></div>
+    </StyledContainer>
   )
 }
 
